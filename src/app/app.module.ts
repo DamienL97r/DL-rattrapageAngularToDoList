4e871a25130a1196e1795f3ad22a59283a4a22c3
Damien Lebon <damienlebon.ifpa@gmail.com>
2023-12-08 18:57:39 +0100

    create routes and component of ToDolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TachesComponent } from './taches/taches.component';
+import { CreateTachesComponent } from './create-taches/create-taches.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TachesComponent,
+    CreateTachesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

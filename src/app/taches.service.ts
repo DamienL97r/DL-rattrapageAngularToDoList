6f619fdb6527be9f7b10ffeaa98ab91bee7e6645
Damien Lebon <damienlebon.ifpa@gmail.com>
2023-12-09 03:25:09 +0100

    manage to display item of ToDoList on homePage

--- src/app/taches.service.ts
+++ src/app/taches.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { mockTaches } from './mock-taches';
+import { ITask } from './itask';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TachesService {
 
+  toDoList: ITask[] = mockTaches;
+
   constructor() { }
+
+  //Lire tous les points de la to do list
+  fetchAll() {
+    return this.toDoList;
+  }
+
+  addTask(newTask: any) {
+
+    // Push la nouvelle task dans le tableau toDoList
+    this.toDoList.push(newTask);
+  }
 }
--- src/app/taches/taches.component.ts
+++ src/app/taches/taches.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TachesService } from '../taches.service';
+import { ITask } from '../itask';
 
 @Component({
   selector: 'app-taches',
   templateUrl: './taches.component.html',
   styleUrls: ['./taches.component.css']
 })
-export class TachesComponent {
+export class TachesComponent implements OnInit {
+
+  constructor(private taskService: TachesService) {}
+
+  tasks: ITask[] = [];
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.fetchAll();
+  }
 
 }

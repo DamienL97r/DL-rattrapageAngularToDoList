baf9c7f1b51bf1e6dbabb107c839b36bca313d01
Damien Lebon <damienlebon.ifpa@gmail.com>
2023-12-09 01:58:39 +0100

    create mock taches

---
+++ src/app/mocks/mock-taches.ts
@@ -0,0 +1,14 @@
+export const mockTaches = [
+    {
+    "id": 1,
+    "titre": "Acheter du lait",
+    },
+    {
+    "id": 2,
+    "titre": "Répondre aux e-mails",
+    },
+    {
+    "id": 3,
+    "titre": "Appeler le plombier",
+    }
+];
